38fad13a2cbe4e645a011eaf4e2218f1d42bbc8e
Rodrigo Xavier <rodrfx@gmail.com>
2022-07-04 13:04:41 -0300

    Recebe: array de numeros (a), remova o 3 primeiros elementos de a e retorne o resultado

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-// Write a function that takes an object with two properties and a string as arguments
-// It should return the value of the property with key equal to the value of the string
-function myFunction(obj, key) {
-	return obj[key];
+// Write a function that takes an array (a) as argument
+// Remove the first 3 elements of 'a'
+// Return the result
+function myFunction(a) {
+	return a.slice(3);
 }
 
-const result = myFunction({ continent: 'Asia', country: 'Japan' }, 'continent');
+const result = myFunction([5, 4, 3, 2, 1, 0]);
 console.log(result);
