89cdf517e690f5bd25a83d94c95f8c818bd4535f
Rodrigo Xavier <rodrfx@gmail.com>
2022-08-27 13:44:45 -0300

    Recebe um array e um numero, deve retornar os ultimos elementos do array com base no numero passado

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-// Write a function that takes an object (a) and a string (b) as argument
-// Return true if the object has a property with key 'b', but only if it has a truthy value
-// In other words, it should not be null or undefined or false
-// Return false otherwise
-
-function myFunction(a, b) {
-	return Boolean(a[b]);
+// Write a function that takes an array (a) and a number (n) as arguments
+// It should return the last n elements of a
+function myFunction(a, n) {
+	return a.splice(-n);
 }
 
-const result = myFunction({ x: 'a', b: 'b', z: undefined }, 'z');
+const result = myFunction([1, 2, 3, 4, 5], 2);
 console.log(result);
